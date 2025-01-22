5afa86808889e4efcd129a9499f48550c335983f
itselcid <itselcid01@gmail.com>
2025-01-22 16:24:15 +0100

    refactor: use std::cout instead of using directive for clarity

--- Cpp-00/ex00/megaphone.cpp
+++ Cpp-00/ex00/megaphone.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 
-using std::cout;
+
 int main(int argc, char **argv)
 {
     if (argc == 1)
     {
-        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE * \n";
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE * \n" ;
         return 0;
     }
     else
     {
-        int i=1;
+        int i = 1;
 
-        while(i<argc)
+        while (i < argc)
         {
             int j = 0;
             while (argv[i][j])
             {
-                cout << char(toupper(argv[i][j]));
+                std::cout << char(toupper(argv[i][j]));
                 j++;
             }
             i++;
         }
-        cout <<"\n";
+        std::cout << "\n";
     }
 }
