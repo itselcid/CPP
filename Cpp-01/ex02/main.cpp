473374e2ce92eeb62156affe87fd556c84619a7a
itselcid <its.elcid01@gmail.com>
2025-01-26 18:18:46 +0100

    refactor: improve formatting and output for string pointer and reference demonstration

--- Cpp-01/ex02/main.cpp
+++ Cpp-01/ex02/main.cpp
@@ -1,10 +1,17 @@
-#include<iomanip>
-#include<iostream>
-
+#include <iomanip>
+#include <iostream>
 
 int main()
 {
-	std::string A="HI THIS IS BRAIN";
-	std::string* stringPTR = &A;
-	std::string& stringREF= A;
+	std::string A = "HI THIS IS BRAIN";
+	std::string *stringPTR = &A;
+	std::string &stringREF = A;
+
+	std::cout << &A << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << A << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 }
