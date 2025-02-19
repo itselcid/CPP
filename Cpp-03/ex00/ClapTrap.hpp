82ba3b7ee0650eb796b1df000ce9b228a155b4c9
itselcid <its.elcid01@gmail.com>
2025-02-19 17:42:20 +0100

    feat: implement ClapTrap class with constructors, destructors, and member functions

--- Cpp-03/ex00/ClapTrap.hpp
+++ Cpp-03/ex00/ClapTrap.hpp
@@ -2,6 +2,7 @@
 #define CLAPTRAP_HPP
 
 #include <string>
+#include <iostream>
 
 class ClapTrap
 {
@@ -12,8 +13,14 @@ class ClapTrap
     unsigned int attack_damage;
 
 public:
+    ClapTrap(std::string _name);
+    ClapTrap(const ClapTrap &obj);
+    ClapTrap &operator=(const ClapTrap &obj);
+    ~ClapTrap();
     void attack(const std::string &target);
     void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    
 };
 
 #endif
--- Cpp-03/ex00/main.cpp
+++ Cpp-03/ex00/main.cpp
@@ -1,1 +1,27 @@
-Name=ClapTrap
+#include "ClapTrap.hpp"
+
+int main()
+{
+
+    ClapTrap clap1("R2D2");
+    ClapTrap clap2(clap1);
+    clap1.attack("C3PO");
+    clap1.takeDamage(5);    
+    clap1.beRepaired(3);   
+    for (int i = 1; i <= 11; i++)
+    {
+        std::cout << "Attack attempt " << i << ": ";
+        clap1.attack("Target");
+    }
+    
+    clap1.beRepaired(5);   
+    clap2.takeDamage(15);   
+    clap2.attack("Target");
+    clap2.beRepaired(5);   
+    
+    return 0;
+}
+
+
+
+
