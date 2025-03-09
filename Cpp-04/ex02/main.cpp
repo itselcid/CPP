6fdaefdaf15362a16547eeab27b598a46542b5da
itselcid <its.elcid01@gmail.com>
2025-03-09 04:25:20 +0000

    refactor: simplify main function by removing unused animal array and comments

--- Cpp-04/ex02/main.cpp
+++ Cpp-04/ex02/main.cpp
@@ -6,33 +6,18 @@
 
 int main()
 {
-    const int count = 4;
-    Animal* animals[count];
-    
-    // Fill array with half dogs and half cats
-    for(int i = 0; i < count; i++)
-    {
-        if(i < count/2)
-            animals[i] = new Dog();
-        else
-            animals[i] = new Cat();
-    }
 
-    // Test deep copy for Dog
     Dog* originalDog = new Dog();
-    Dog* copyDog = new Dog(*originalDog);  // Deep copy test
-    delete originalDog;   // Original can be deleted and copy should still work
+    Dog* copyDog = new Dog(*originalDog);  
+    delete originalDog;   
     delete copyDog;
 
-    // Test deep copy for Cat
     Cat* originalCat = new Cat();
-    Cat* copyCat = new Cat(*originalCat);  // Deep copy test
-    delete originalCat;   // Original can be deleted and copy should still work
+    Cat* copyCat = new Cat(*originalCat); 
+    delete originalCat; 
     delete copyCat;
 
-    // Delete the array of animals
-    for(int i = 0; i < count; i++)
-        delete animals[i];
+
 
     return 0;
 }
