2c82bf5d629bc44a0adb144e766ad1ef1d8269f0
itselcid <its.elcid01@gmail.com>
2025-04-18 22:53:03 +0100

    done maybe

--- Cpp-07/ex02/main.cpp
+++ Cpp-07/ex02/main.cpp
@@ -1,56 +1,83 @@
 #include <iostream>
+#include <string> 
 #include "Array.hpp"
+
+
 #include "Array.tpp"
-#include <cstdlib> 
-#include <ctime>
 
-#define MAX_VAL 750
-int main(int, char**)
+int main( void )
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+    
+    Array<std::string> stringArray(10);
+    Array<std::string> otherStringArray;
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
+    stringArray[0] = "Hi";
+    stringArray[1] = "Hello";
+    stringArray[2] = "its";
+    stringArray[3] = "me";
+    stringArray[4] = "omar";
+
+    std::cout << "stringArray.size() = " << stringArray.size() << std::endl; 
+
+    otherStringArray = stringArray; 
+    otherStringArray[3] = "I'm good, how about you?"; 
+
+    std::cout << "--- Original stringArray ---" << std::endl;
+    std::cout << stringArray[0] << std::endl;
+    std::cout << stringArray[1] << std::endl;
+    std::cout << stringArray[2] << std::endl;
+    std::cout << stringArray[3] << std::endl; 
+    std::cout << stringArray[4] << std::endl;
+
+    std::cout << "\n--- Modified otherStringArray ---" << std::endl;
+    std::cout << otherStringArray[3] << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "--- Testing Array<int> ---" << std::endl;
+    Array<int> intArray(5);
+    Array<int> otherIntArray;
+
+    intArray[0] = 10;
+    intArray[1] = 20;
+    intArray[2] = 30;
+    intArray[3] = 40;
+    intArray[4] = 50;
+
+    std::cout << "intArray.size() = " << intArray.size() << std::endl; 
+
+    otherIntArray = intArray; 
+    otherIntArray[1] = 25;    
+
+    std::cout << "Original intArray[0]: " << intArray[0] << std::endl; 
+    std::cout << "Original intArray[1]: " << intArray[1] << std::endl; 
+     std::cout << "otherIntArray[0]: " << otherIntArray[0] << std::endl;
+    std::cout << "Modified otherIntArray[1]: " << otherIntArray[1] << std::endl;
+   
+    std::cout << std::endl;
+
+    std::cout << "--- Testing exceptions ---" << std::endl;
     try
     {
-        numbers[-2] = 0;
+        std::cout << "Accessing stringArray[59]: ";
+        std::cout << stringArray[59] << std::endl; 
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
+        std::cout << e.what() << std::endl;
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
+    try
     {
-        numbers[i] = rand();
+        std::cout << "Accessing intArray[-3]: ";
+        std::cout << intArray[-3] << std::endl; 
     }
-    delete [] mirror;//
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+
+
     return 0;
 }
